fd995a7a3294d724d2e6fdfeb220b2f83c104de0
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-17 09:49:51 +0200

    Виконанано Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const INGRADIENTS = document.querySelector("#ingredients");
+const items = [];
+
+ingredients.forEach((ingradient, i) => {
+  items.push(document.createElement("li"));
+  items[i].textContent = ingradient;
+  items[i].classList.add("item");
+});
+
+INGRADIENTS.append(...items);
+
