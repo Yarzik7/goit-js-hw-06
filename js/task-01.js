cd436ba33b14d7d2eb5ef8a2807b6eadd14a362e
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-17 09:00:37 +0200

    Рефакторинг 1
    Умова для переносу строки

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,17 @@
-const categoriesList = document.querySelector("#categories");
+const categoriesCount = document.querySelector("#categories").children.length;
 const categoriesItem = document.querySelectorAll(".item"); //categoriesList.children;//document.querySelectorAll("#categories item");
-const categoryNames = document.querySelectorAll("h2"); //categoriesItem.childNodes;
+//const categoryNames = document.querySelectorAll("h2"); //categoriesItem.childNodes;
 
-const categoriesArray = categoriesList.children;
+//const categoriesArray = categoriesList.children;
 
-console.log("Numbers of categories: ", categoriesArray.length, "\n\n");
+console.log("Numbers of categories: ", categoriesCount, "\n\n");
+//console.log(categoriesCount);
 //console.log(categoriesItem);
 //console.log(categoryNames);
 //categoryNames.forEach((name) => console.log("Category: ",name.innerText));
 
-categoriesItem.forEach((item) => {
+categoriesItem.forEach((item, i) => {
   console.log("Category: ", item.querySelector("h2").innerText);
-  console.log("Elements: ", item.querySelector("ul").children.length, "\n\n");
+  console.log("Elements: ", item.querySelector("ul").children.length);
+  if (i < categoriesItem.length - 1) console.log("\n");
 });
-//const categoryNames = categoriesArray.map(category => category)
