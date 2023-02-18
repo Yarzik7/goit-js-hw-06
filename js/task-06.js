1bbdf139b344d2353f3ca4c6fa280c9f124e83e8
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-18 12:08:19 +0200

    Дороблено Task 6

--- js/task-06.js
+++ js/task-06.js
@@ -4,10 +4,12 @@ const validCountChar = Number(input.dataset.length);
 const blur = event => {
   const countChar = event.currentTarget.value.length;
   if (countChar === validCountChar) {
+    if (input.classList.value.includes('invalid')) input.classList.remove('invalid');
     input.classList.add('valid');
     return;
   }
 
+  if (input.classList.value.includes('valid')) input.classList.remove('valid');
   input.classList.add('invalid');
 };
 
