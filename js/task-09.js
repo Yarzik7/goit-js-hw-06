a4c86d9f69b9b26ef4a2528c02611a3c002359e5
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-19 06:00:20 +0200

    Виконано Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const btnChange = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+const changeColor = () => {
+  const generatedColor = getRandomHexColor();
+
+  body.style.backgroundColor = generatedColor;
+  spanColor.innerText = generatedColor;
+};
+
+btnChange.addEventListener('click', changeColor);
