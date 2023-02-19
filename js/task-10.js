dd00700e81c7911a4fb3fce3c365c5ae536cbe7c
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-19 14:12:59 +0200

    Виконано Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -7,15 +7,7 @@ const btnCreate = document.querySelector('button[data-create]');
 const btnDestroy = document.querySelector('button[data-destroy]');
 const boxes = document.querySelector('#boxes');
 
-inputNumber.value = 5;
-
 let boxSize = 30;
-
-// function getNumber() {
-//   inputValue = inputNumber.value;
-//   console.log("gN",inputValue);
-//   return inputNumber.value;
-// }
 
 function callCreateBoxes() {
   createBoxes(Number(inputNumber.value));
@@ -28,26 +20,14 @@ const createBoxes = amount => {
       `<div style="background-color: ${getRandomHexColor()}; width: ${boxSize}px; height: ${boxSize}px;"></div>`
     );
   }
-
   boxes.insertAdjacentHTML('beforeend', marcupBoxes.join(''));
-  //console.log(marcupBoxes);
-  //console.log(amount);
-  //console.log('n', inputValue);
 };
 
 const destroyBoxes = () => {
   boxes.innerHTML = '';
+  inputNumber.value = '';
   boxSize = 30;
 };
 
-//const callCreateBoxes = createBoxes.bind(inputNumber, inputNumber.value);
-
-//const create = createBoxes.bind();
-
-//inputNumber.addEventListener('input', getNumber.bind(inputNumber));
-
 btnCreate.addEventListener('click', callCreateBoxes);
 btnDestroy.addEventListener('click', destroyBoxes);
-
-//btnCreate.addEventListener('click', createBoxes.bind(btnCreate, getNumber.call(inputNumber)));
-//btnCreate.addEventListener('click', createBoxes.bind(btnCreate, getNumber.bind(inputNumber)));
