c71a334a3b00a8d1759bd6e088ebf6a24a66dbfb
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-19 05:44:27 +0200

    Виконано Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+const handleSubmit = event => {
+  event.preventDefault();
+  // const {
+  //   elements: { email, password },
+  // } = event.currentTarget;
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (!email.value || !password.value)
+    return alert('В формі є незаповнені поля. Будь ласка заповніть всі поля!');
+
+  const loginFormObject = {
+    [email.name]: email.value,
+    [password.name]: password.value,
+  };
+
+  console.log(loginFormObject);
+  event.currentTarget.reset();
+};
+
+form.addEventListener('submit', handleSubmit);
