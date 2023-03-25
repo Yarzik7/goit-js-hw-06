c17fcba2ffc794771746900bfc64b9ae8f75884b
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-03-25 14:53:29 +0200

    Оновлено №5

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,6 @@
 const inputEl = document.querySelector('#name-input');
 const spanEl = document.querySelector('#name-output');
 
-const inputEvent = ({ currentTarget: { value } }) => value ? (spanEl.textContent = value) : (spanEl.textContent = 'Anonymous');
+const inputEvent = ({ currentTarget: { value } }) => value ? spanEl.textContent = value : spanEl.textContent = 'Anonymous';
 
 inputEl.addEventListener('input', inputEvent);
