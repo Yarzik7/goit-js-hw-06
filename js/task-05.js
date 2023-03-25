4b06555325eae89dc7cd349bd719b85fc18c0b76
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-03-25 14:34:59 +0200

    Рефакторинг для №5

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,12 @@
 const inputEl = document.querySelector('#name-input');
 const spanEl = document.querySelector('#name-output');
 
-const inputEvent = event => {
-  spanEl.textContent = event.currentTarget.value;
-  if (!spanEl.textContent) spanEl.textContent = 'Anonymous';
+const inputEvent = ({ currentTarget: { value } }) => {
+  if (!value) {
+    spanEl.textContent = 'Anonymous';
+    return;
+  }
+  spanEl.textContent = value;
 };
 
 inputEl.addEventListener('input', inputEvent);
